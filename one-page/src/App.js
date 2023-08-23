7742c677d37ae1579ac6e17043964c7599867f38
Shao Flu <304402714@qq.com>
2023-08-23 08:58:06 +0000

    添加 React 库

--- one-page/src/App.js
+++ one-page/src/App.js
@@ -1,18 +1,29 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header>
-      </header>
+      <header></header>
       {getHeaderDiv()}
     </div>
   );
 }
 function getHeaderDiv() {
-  return <div style={{width: '100vw',height: '60px', margin: '0 auto' , backgroundColor: '#756472', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-    <h2 style={{textAlign: "center",color:'white'}}> 标题 </h2>
-</div>
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "60px",
+        margin: "0 auto",
+        backgroundColor: "#756472",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h2 style={{ textAlign: "center", color: "white" }}> 标题 </h2>
+    </div>
+  );
 }
 
 export default App;
